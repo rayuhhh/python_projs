afc349a0dae0357b66ac733f81f74168b38922fa
rayr <rayuhhh@gmail.com>
2025-05-06 11:21:34 -0500

    base template html for the todo app, also render html

--- todo/app.py
+++ todo/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!123"
+    return render_template('base.html')
+
+# @app.route('/about')
+# def about():
+#     return "About"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
